eed7c5e5fb4a0f658f55506946a150116a32a789
Zayn Baig <zayn.baig@gmail.com>
2023-08-07 18:54:12 -0400

    Tweaks the color of the header.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,10 @@
-import { ModeToggle } from "./mode-toggle";
+import { ModeToggle } from "@/components/mode-toggle";
 import { mate_sc } from "@/lib/fonts";
 
 const Header = () => {
     return (
-        <header className={`${mate_sc.className} bg-accent flex justify-between items-center px-8 py-2`}>
-            <div className="scroll-m-20 border-b pb-2 text-4xl font-semibold tracking-wide transition-colors first:mt-0">Stonks</div>
+        <header className={`${mate_sc.className} flex justify-between items-center px-8 py-2 border-b-2`}>
+            <div className="scroll-m-20 pb-2 text-4xl font-semibold tracking-wide transition-colors first:mt-0">Stonks</div>
             <div className="flex items-center">
                 <ModeToggle />
             </div>
